3f2a048e4e5bd11818e98d1189a14d19206a4d9a
Felipe Possari <fcpfelipe@hotmail.com>
2021-03-06 10:41:25 -0300

    Parser class added and transactions parsed

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/Solucao.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/Solucao.java
@@ -1,41 +1,51 @@
 package br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.modifique;
 
+import br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.pronto.DadosTransacao;
+
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solucao {
 
-	/**
-	 * 
-	 * @param infoTransacoes dados das transações. A String está formatada da seguinte maneira:		
-		<b>"valor,metodoPagamento,numeroCartao,nomeCartao,validade,cvv,idTransacao"</b>
-		<ol>
-		 <li> Valor é um decimal</li>
-	 	 <li> O método de pagamento é 'DEBITO' ou 'CREDITO' </li>
-	 	 <li> Validade é uma data no formato dd/MM/yyyy. </li>
-	 	</ol>
-	 	
-	 * @param infoAdiantamentos informacao da transacao que pode ser recebida adiantada. A String está formatada da seguinte maneira:		
-		<b>"idTransacao,taxa"</b>
-		<ol>
-	 	 <li> Taxa é um decimal </li>	 	 
-	 	</ol> 
-	 * 
-	 * @return Uma lista de array de string com as informações na seguinte ordem:
-	 * [status,valorOriginal,valorASerRecebidoDeFato,dataEsperadoRecebimento].
-	 * <ol>
-	 *  <li>O status pode ser 'pago' ou 'aguardando_pagamento'</li>
-	 *  <li>O valor original e o a ser recebido de fato devem vir no formato decimal. Ex: 50.45</li>
-	 *  <li>dataEsperadoRecebimento deve ser formatada como dd/MM/yyyy. Confira a classe {@link DateTimeFormatter}</li> 
-	 * </ol> 
-	 * 
-	 * É esperado que o retorno respeite a ordem de recebimento
-	 */
-	public static List<String[]> executa(List<String> infoTransacoes, List<String> infoAdiantamentos) {
-		
-		return List.of(new String[][] { 
-					 {"pago","200","194","04/03/2021"} 					 
-					}); 
-	}
+    private static final TransacaoParser parser = new TransacaoParser();
+
+    /**
+     * @param infoTransacoes    dados das transações. A String está formatada da seguinte maneira:
+     *                          <b>"valor,metodoPagamento,numeroCartao,nomeCartao,validade,cvv,idTransacao"</b>
+     *                          <ol>
+     *                          <li> Valor é um decimal</li>
+     *                          <li> O método de pagamento é 'DEBITO' ou 'CREDITO' </li>
+     *                          <li> Validade é uma data no formato dd/MM/yyyy. </li>
+     *                          </ol>
+     * @param infoAdiantamentos informacao da transacao que pode ser recebida adiantada. A String está formatada da seguinte maneira:
+     *                          <b>"idTransacao,taxa"</b>
+     *                          <ol>
+     *                          <li> Taxa é um decimal </li>
+     *                          </ol>
+     * @return Uma lista de array de string com as informações na seguinte ordem:
+     * [status,valorOriginal,valorASerRecebidoDeFato,dataEsperadoRecebimento].
+     * <ol>
+     *  <li>O status pode ser 'pago' ou 'aguardando_pagamento'</li>
+     *  <li>O valor original e o a ser recebido de fato devem vir no formato decimal. Ex: 50.45</li>
+     *  <li>dataEsperadoRecebimento deve ser formatada como dd/MM/yyyy. Confira a classe {@link DateTimeFormatter}</li>
+     * </ol>
+     * <p>
+     * É esperado que o retorno respeite a ordem de recebimento
+     */
+    public static List<String[]> executa(List<String> infoTransacoes, List<String> infoAdiantamentos) {
+        List<DadosTransacao> transacaos = parseTransactions(infoTransacoes);
+
+
+        return List.of(new String[][]{
+                {"pago", "200", "194", "04/03/2021"}
+        });
+    }
+
+    private static List<DadosTransacao> parseTransactions(List<String> infoTransacoes) {
+        return infoTransacoes.stream().
+                map(x -> parser.parseTransaction(x))
+                .collect(Collectors.toList());
+    }
 
 }
---
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/TransacaoParser.java
@@ -0,0 +1,36 @@
+package br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.modifique;
+
+import br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.pronto.DadosRecebimentoAdiantado;
+import br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.pronto.DadosTransacao;
+import br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.pronto.MetodoPagamento;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TransacaoParser {
+
+    private static final String REGEX = ",";
+    private static final int INDEX_VALOR = 0;
+    private static final int INDEX_METODO_PAGAMENTO = 1;
+    private static final int INDEX_NUMERO_CARTAO = 2;
+    private static final int INDEX_NOME_CARTAO = 3;
+    private static final int INDEX_VALIDADE = 4;
+    private static final int INDEX_CVV = 5;
+    private static final int INDEX_ID_TRANSACAO = 6;
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public DadosTransacao parseTransaction(String transacao) {
+        String dados[] = transacao.split(REGEX);
+
+        return new DadosTransacao(
+                new BigDecimal(dados[INDEX_VALOR]),
+                MetodoPagamento.valueOf(dados[INDEX_METODO_PAGAMENTO]),
+                dados[INDEX_NUMERO_CARTAO],
+                dados[INDEX_NOME_CARTAO],
+                LocalDate.parse(dados[INDEX_VALIDADE], dateFormatter),
+                Integer.parseInt(dados[INDEX_CVV]),
+                Integer.parseInt(dados[INDEX_ID_TRANSACAO])
+        );
+    }
+}
